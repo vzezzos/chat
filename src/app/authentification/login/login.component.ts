86fed6fd9a6c040ceafa8f51ac644e560f8569dc
unknown <sbenning@student.42.fr>
2019-01-28 21:25:40 +0100

    modification class user

--- src/app/authentification/login/login.component.ts
+++ src/app/authentification/login/login.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input, OnChanges } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { AuthentificationService } from '../../providers/authentification.service';
 import { Router } from '@angular/router';
@@ -11,9 +11,9 @@ import { User } from 'src/app/models/user';
 })
 export class LoginComponent implements OnInit {
 
-  _userToLog: any;
+  _userToLog: User;
   @Input()
-  set userToLog(user: any) {
+  set userToLog(user: User) {
     this._userToLog = user;
     console.log('DANS LE SETTER', user);
     if (this.username && this.mdp) {
@@ -21,7 +21,7 @@ export class LoginComponent implements OnInit {
       this.mdp.setValue(this._userToLog.password);
     }
   }
-  get userToLog(): any {
+  get userToLog(): User {
     return this._userToLog;
   }
 
@@ -50,99 +50,4 @@ export class LoginComponent implements OnInit {
     this.flagCo = false;
   }
 
-  checkIfCorrect() {
-    this.flagUser = !!this.authentification.findUser(this.username, this.mdp);
-    
-    if (this.flagUser) {
-      //this.authentification.updateUser(this.flagUser);
-      setTimeout(() => {
-        this.router.navigate(['/home']);
-      }, 0);
-    }
-    else {
-      this.flagCo = true;
-    }
-  }
-
 }
-/*
-import { Component, OnInit, OnDestroy } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
-import { AuthentificationService } from '../authentification.service';
-import { Subscription } from 'rxjs';
-import { Router } from '@angular/router';
-
-
-@Component({
-  selector: 'app-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
-})
-export class LoginComponent implements OnInit, OnDestroy {
-
-  loginForm: FormGroup;
-  username: FormControl;
-  mdp: FormControl;
-  flagUser: boolean;
-  flagCo: boolean;
-  subscription :Subscription;
-
-  constructor(public router: Router, public authentification: AuthentificationService) {
-  }
-
-  ngOnInit() {
-    this.subscription = this.authentification.flagUser$.subscribe((flag: boolean) => {
-      if (!flag) {
-        this.username = new FormControl('coucoutoi', [
-          Validators.required,
-          Validators.minLength(4),
-        ]);
-        this.mdp = new FormControl('coucoutoi', [
-          Validators.required,
-          Validators.minLength(8),
-        ]);
-        this.loginForm = new FormGroup({
-          username: this.username,
-          mdp: this.mdp,
-        });
-        this.flagUser = this.authentification.flagUser;
-        this.flagCo = false;
-      } else {
-        this.username = new FormControl('');
-        this.mdp = new FormControl('');
-        this.loginForm = new FormGroup({
-          username: this.username,
-          mdp: this.mdp,
-        });
-        setTimeout(() => {
-          this.router.navigate(['/home']);
-        }, 0);
-      }
-    })
-  }
-
-  ngOnDestroy() {
-    if (this.subscription && this.subscription.closed === false) {
-      this.subscription.unsubscribe();
-    }
-  }
-
-  checkIfCorrect() {
-    this.flagUser = !!this.authentification.findUser(this.username, this.mdp);
-    
-    if (this.flagUser) {
-      this.authentification.updateUser(this.flagUser);
-    }
-    else {
-      this.flagCo = true;
-    }
-  }
-  
-  deconnexionUser() {
-    this.loginForm.reset();
-    this.flagUser = false;
-    this.flagCo = false;
-    this.authentification.updateUser(false);
-  }
-}
-*/
--- src/app/models/user.ts
+++ src/app/models/user.ts
@@ -1,25 +1,13 @@
-import * as Crypto from 'crypto-js';
-
-export const key: string = 'Monmotde!!Passssssserapas';
 
 export class User {
 
     constructor(
-        public login: string,
+        public username: string,
         public admin: boolean,
         public id: string,
-        public mdp: string
+        public password: string
 
     ) {
-        //this.mdp = Crypto.AES.encrypt(mdpRef, key);
-    }
-/*
-    addLog(logsList: User[]) {
-        return logsList.push();
     }
 
-    removeLog(logsList: User[]) {
-        return logsList.pop();
-    }
-*/
 }
